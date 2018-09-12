af065b067c045532e1990c51bab6fe43210a8d78
Amit5284 <40840515+AmitSajane@users.noreply.github.com>
2018-09-13 00:59:05 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -20,7 +20,7 @@ const Footer = () => {
                 <Fa icon="codepen" className="mr-3"/>
             </div>
             <p className="footer-copyright mb-0 py-3 text-center">
-                &copy; {new Date().getFullYear()} Copyright: <a href="https://www.MDBootstrap.com"> MDBootstrap.com </a>
+                &copy; {new Date().getFullYear()} Copyright: <a href="https://www.MDBootstrap.com">  </a>
             </p>
         </MDBFooter>
     );
